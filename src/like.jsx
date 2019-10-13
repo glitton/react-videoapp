1ee570220dac30f93e253a4cce34fde5807812f0
Generosa Litton <glcodeworks@gmail.com>
2019-10-13 16:22:12 -0700

    like appears need to make it dynamic

--- src/like.jsx
+++ src/like.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 class Like extends Component {
+  // let classes = "fa-fa-heart-o";
+  // if (!this.props.like) classes += "-o";
   render() {
-    // let classes = "fa-fa-heart-o";
-    // if (!this.props.like) classes += "-o";
-    return <i className="fa-fa-heart" aria-hidden="true"></i>;
+    return <i className="fa fa-heart"></i>;
   }
 }
 
