db7c9cb9b9749bde87331f76c93fd31774eaee9d
Generosa Litton <glcodeworks@gmail.com>
2019-12-14 18:02:03 -0800

    added prop types

--- src/pagination.jsx
+++ src/pagination.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash"; //lodash comes from underscore
 
 const Pagination = props => {
@@ -25,6 +26,13 @@ const Pagination = props => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
